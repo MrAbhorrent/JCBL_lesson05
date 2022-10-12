7996668a68ddc5cd134140c785427652c7ff4264
NAG <MrAbhorrent@yandex.ru>
2022-10-12 22:04:23 +0500

    Заполнение массива вынесено в отдельный метод

--- src/ru/geekbrains/Main.java
+++ src/ru/geekbrains/Main.java
@@ -3,20 +3,24 @@ package ru.geekbrains;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Домашне задание 5");
+
+        System.out.println("Домашнее задание 5");
         System.out.println("Создаем список сотрудников...");
         Employee[] employeers = new Employee[5];
-        employeers[0] = new Employee("Иван", "Поддубный", "Стажер","poddubnyi@geekbrains.ru", 89012345678l, 25000, 28);
-        employeers[1] = new Employee("Никита", "Кожемяка", "Программист","kozhemiakan@geekbrains.ru", 89023456781l, 47000, 42);
-        employeers[2] = new Employee("Добрыня", "Панкратов", "Зав. складом","pankratovd@geekbrains.ru", 89034567891l, 23000, 45);
-        employeers[3] = new Employee("Светлана", "Худая", "Бухгалтер","hoodayas@geekbrains.ru", 89337891234l, 56000, 34);
-        employeers[4] = new Employee("Ганнибал", "Мокрый", "Начальник","mokryig@geekbrains.ru", 89221122679l, 89000, 53);
-
+        loadData(employeers);
         System.out.println("Вывводим сотрудников старше 40 лет...");
-        for (Employee employee1: employeers
-             ) {
+        for (Employee employee1: employeers) {
             if (employee1.getAge() >= 40) System.out.println(employee1.info());
+        }
+    }
+
+    private static void loadData(Employee[] array) {
+        if (array.length == 5) {
+            array[0] = new Employee("Иван", "Поддубный", "Стажер","poddubnyi@geekbrains.ru", 89012345678L, 25000, 28);
+            array[1] = new Employee("Никита", "Кожемяка", "Программист","kozhemiakan@geekbrains.ru", 89023456781L, 47000, 42);
+            array[2] = new Employee("Добрыня", "Панкратов", "Зав. складом","pankratovd@geekbrains.ru", 89034567891L, 23000, 45);
+            array[3] = new Employee("Светлана", "Худая", "Бухгалтер","hoodayas@geekbrains.ru", 89337891234L, 56000, 34);
+            array[4] = new Employee("Ганнибал", "Мокрый", "Начальник","mokryig@geekbrains.ru", 89221122679L, 89000, 53);
         }
     }
 }
